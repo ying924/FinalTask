98655dde8f3aa8fc9e029d5baa0c5a38db59fc37
ying924 <83344992+ying924@users.noreply.github.com>
2021-10-01 13:29:52 +0800

    Add files via upload

---
+++ components/ProductListItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="detail">
+
+        <table cellspacing="5" cellpadding="5">
+          <template  v-for="p in products" :key="p.name">
+            <tr>
+                <th >
+                    <span><img class="resultImage" :src="p.img" /></span>
+                </th>
+                <th>
+                    <span>{{p.name}}</span>
+                </th>
+                <th>
+                    <span> $ {{p.price}}</span>
+                </th>
+            </tr>
+            </template>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "productListItems",
+  props: ['products']
+}
+</script>
+
+<style>
+.resultImage {
+    border-radius: 8px;
+    border: 1px solid #ddd;
+    padding: 5px;
+    max-width: 30%;
+}
+</style>
